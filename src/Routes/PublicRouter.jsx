c39a3edd4e392a9e638bf15f2f36cd009dabc1fd
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-24 13:25:00 -0500

    proteger rutas URL

--- src/Routes/PublicRouter.jsx
+++ src/Routes/PublicRouter.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Route } from "react-router";
+import { Redirect, Route } from "react-router";
 
 const PublicRouter = ({ auth,component: Component, ...rest }) => {
   return (
     <>
-      <Route {...rest} component={() => <Component />} />
+      <Route {...rest} component={(props) => !auth.log ? <Component {...props}/> : <Redirect to="/" />} />
     </>
   );
 };
