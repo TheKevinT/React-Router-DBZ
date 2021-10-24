a09d84475a09201c868b52575436880147096f44
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-24 11:11:15 -0500

    pantalla de search dar estilo y crear formulario

--- src/Pages/SearchScreen.jsx
+++ src/Pages/SearchScreen.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SearchScreen = () => {
-    return (
-        <>
-            <h1>Search</h1>
-        </>
-    )
-}
+const SearchScreen = ({ history }) => {
+  const [inputValue, setInputValue] = useState("");
 
-export default SearchScreen
+  const _handleChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`?search=${inputValue}`);
+  };
+
+  return (
+    <>
+      <div className="container mt-3">
+        <h1 className="text-center">Search</h1>
+        <div className="row">
+          <div className="col-6">
+            <form onSubmit={_handleSubmit}>
+              <label className="form-label w-100">
+                Character:
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Name Character"
+                  autoComplete="off"
+                  value={inputValue}
+                  onChange={_handleChange}
+                />
+              </label>
+              <button type="submit" className="btn btn-success w-100">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchScreen;
