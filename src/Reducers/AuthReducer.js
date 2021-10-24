8999ba6b5addc98339811a47d0f271695fa77094
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-24 12:04:19 -0500

    rutas privadas, context, type, reducer

---
+++ src/Reducers/AuthReducer.js
@@ -0,0 +1,12 @@
+import { authTypes } from "../Types/authTypes";
+
+export const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case authTypes.login:
+      return { log: true };
+    case authTypes.logout:
+      return { log: false };
+    default:
+      return state;
+  }
+};
---
+++ src/Routes/PrivateRouter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router";
+
+const PrivateRouter = ({ component: Component }) => {
+  return (
+    <>
+      <Route component={<Component />} />
+    </>
+  );
+};
+
+export default PrivateRouter;
