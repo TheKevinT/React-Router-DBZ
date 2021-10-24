3c01ba52845d74f5a2a7c671cae1328780860fc2
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-24 12:33:11 -0500

    usar reducer y context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import LoginRouter from "./Routes/LoginRouter";
+import { AuthContext } from "./Context/AuthContext";
+import { AuthReducer } from "./Reducers/AuthReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("log")) || { log: false };
+};
 
 const App = () => {
+  const [log, dispatch] = useReducer(AuthReducer, {}, init);
+
+  useEffect(() => {
+    localStorage.setItem("log", JSON.stringify(log));
+  }, [log]);
+
   return (
     <>
-      <LoginRouter />
+      <AuthContext.Provider
+        value={{
+          log,
+          dispatch,
+        }}
+      >
+        <LoginRouter />
+      </AuthContext.Provider>
     </>
   );
 };
